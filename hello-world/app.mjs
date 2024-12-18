96b222cf7464749f608143194bcba401a48a4eb2
georges <georgesdc13@gmail.com>
2024-12-18 15:56:33 -0300

    update: config docker network to connect api serverless

--- hello-world/app.mjs
+++ hello-world/app.mjs
@@ -15,7 +15,7 @@ export const lambdaHandler = async (event, context) => {
     const response = {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'ola mundo!',
+        message: 'ola mundo!!',
       })
     };
 
